9e95f5c19ed1cfdf1e28d15ef4cff743995b2edb
Glitch (collaborative-canvas) <none>
2020-04-25 23:14:29 +0000

    🍗😘 Checkpoint ./public/calligraphy.js:15089704/8076

--- public/calligraphy.js
+++ public/calligraphy.js
@@ -1,7 +1,6 @@
 // Original Code from: BUN
 // https://www.openprocessing.org/sketch/755877
 
-
 var socket = io();
 
 function setup() {
@@ -49,73 +48,45 @@ function wipeCanvas(refreshdata) {
 
 
 function newDrawing(data) {
-  //strokeWeight(3);
-  line(data.x, data.y, data.px, data.py);
   
-  oldR = data.r;
-  if(mouseIsPressed) {
-		
-		line(data.mx, data.my, data.px, data.py);
-    
-    if(!f) {
-     f = 1;
-     x = data.mX;
-     y = data.mY;
-    }
-    
-    ax += ( data.mX - x ) * spring;
-    ay += ( data.mY - y ) * spring;
-    ax *= friction;
-    ay *= friction;
-    a += sqrt( ax*ax + ay*ay ) - data.a;
-    a *= 0.6;
-    r = size - data.a;
-    
-    for( i = 0; i < distance; ++i ) {
-      oldX = data.x;
-      oldY = data.y;
-      x += ax / distance;
-      y += ay / distance;
-      oldR += ( data.r - oldR ) / distance;
-      if(oldR < 1) oldR = 1;
-      strokeWeight( oldR+diff );
-      line( x, y, oldX, oldY );
-      strokeWeight( oldR );
-      line( x+diff*2, y+diff*2, oldX+diff*2, oldY+diff*2 );
-      line( x-diff, y-diff, oldX-diff, oldY-diff );
-			stroke(255);
-    }
-  } else if(f) {
-    ax = ay = f = 0;
-  }
+  strokeWeight( oldR+diff );
+  line( x, y, oldX, oldY );
+
+  strokeWeight( oldR );
+  line( x+diff*2, y+diff*2, oldX+diff*2, oldY+diff*2 );
+  line( x-diff, y-diff, oldX-diff, oldY-diff );
+
+  stroke(255);
   
-  
+  //line(data.x, data.y, data.px, data.py);
+  //line(width - data.x, data.y, width - data.px, data.py);
+  //line(data.x, height - data.y, data.px, height - data.py);
+  //line(width - data.x, height - data.y, width - data.px, height - data.py);
 }
 
 function draw() {
-
-	mX = mouseX;
+  // take out to remove symetry 
+  mX = mouseX;
   mY = mouseY;
   px = pmouseX;
   py = pmouseY;
 	
   oldR = r;
   if(mouseIsPressed) {
-    // mX = mouseX;
-    // mY = mouseY;
 		
-		// following 4 lines added for symetry 
-		line(mx, my, px, py);
+		// following 4 lines added for symetry (take out for no symetry)
+		//line(x, y, px, py);
     //line(width - x, y, width - px, py);
     //line(x, height - y, px, height - py);
     //line(width - x, height - y, width - px, height - py);
     
+    
+    // don't take out 
     if(!f) {
      f = 1;
      x = mX;
-    y = mY;
-    }
-    
+     y = mY;
+   }
     ax += ( mX - x ) * spring;
     ay += ( mY - y ) * spring;
     ax *= friction;
@@ -125,37 +96,30 @@ function draw() {
     r = size - a;
     
     for( i = 0; i < distance; ++i ) {
+      
       oldX = x;
       oldY = y;
-      x += data.ax / distance;
-      y += data.ay / distance;
-      oldR += ( data.r - oldR ) / distance;
+      x += ax / distance;
+      y += ay / distance;
+      
+      oldR += ( r - oldR ) / distance;
       if(oldR < 1) oldR = 1;
+    
+      
+      // code that draws
       strokeWeight( oldR+diff );
-      line( data.x, data.y, oldX, oldY );
+      line( x, y, oldX, oldY );
+      
       strokeWeight( oldR );
-      line( data.x+diff*2, data.y+diff*2, oldX+diff*2, oldY+diff*2 );
-      line( data.x-diff, data.y-diff, oldX-diff, oldY-diff );
+      line( x+diff*2, y+diff*2, oldX+diff*2, oldY+diff*2 );
+      line( x-diff, y-diff, oldX-diff, oldY-diff );
+      
 			stroke(255);
     }
-  } else if(data.f) {
+  } else if(f) {
     ax = ay = f = 0;
   }
 }
 
-function mouseDragged() {
 
-  var data = {
-    mx: mX,
-    my: mY,
-    px: px,
-    py: py,
-    ax: ax,
-    ay: ay,
-    a: a,
-    r: r,
-    f: f
-  };
-  
-  socket.emit("mouse", data);
-}
+
